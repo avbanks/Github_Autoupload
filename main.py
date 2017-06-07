e850e6079a3b8d2f6b2aae8e0a0b9007f5f23195
Andre Banks <andrebanks.ab@gmail.com>
2017-06-07 16:29:04 -0400

    fixed file location

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ import datetime
 
 USERNAME = '#####GITHUB_USERNAME######'
 GITHUB_URL = 'https://github.com/{}'.format(USERNAME)
-DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-
+DIR_PATH = os.path.dirname('/Users/abanks/Projects/Github_AutoUpload/')
+print(DIR_PATH)
+os.chdir(DIR_PATH)
 #Get repo name from hidden config file ignoring '#'
 if not os.path.isfile('./.gitauto.config'):
     print('No config file is here')
     print('Creating config file @ ./.gitauto.config please edit the config')
     #create file here
-
 
 F = open("./.gitauto.config", "r")
 
@@ -26,28 +26,40 @@ for line in F:
             repolist.append(z[0])
 
 filelist = os.listdir(DIR_PATH)
+
 #Check if we want these files updated
 for r in repolist:
     if r in filelist:
         os.chdir(DIR_PATH+'/'+r)
-        subprocess.call(['git', 'add', '.'])
-        if \
-        subprocess.call(['git','commit','-m',str(datetime.datetime.now())])==1:
+        git_add = subprocess.call(['git', 'add', '.'])
+        if git_add != 0:
+            err_comm1 = ('Something went wrong, return value of {}'.format(git_add))
+            subprocess.call('echo', err_comm1)
+            continue
+ 
+        git_commit = subprocess.call(['git','commit','-m',str(datetime.datetime.now())])
+        if git_commit == 0:
+            print('+'*50)
+            print('commiting changes for '+r)
+            print('+'*50)
+
+        elif git_commit == 1:
             print('='*50)
             print(r+' is up-to-date')
             print('='*50)
             continue
         else:
+            print('Something went wrong with the commit, return value of {}'.format(git_add))
+            continue
+ 
+        git_push = subprocess.call(['git', 'push', 'origin', 'master'])
+        if git_push == 0:
             print('+'*50)
-            print('pushing changes for '+r)
-            print('+'*50)
-        try:
-            subprocess.call(['git', 'push', 'origin', 'master'])
-            print('='*50) 
             print(r+' changes pushed to master')
-            print('='*50)
-        except:
+            print('+'*50)
+        else:
             print('/`\`'*50)
-            print("Error: couldn't push commit for "+r)
+            print("Error: couldn't push commit for "+r+' return value of\
+                  {}'.format(git_push))
             print('/`\`'*50)
 
